b06ea7a5aab20e54c101a8113b438e9b3cae64e5
novoross86 <novoross86@gmail.com>
2024-05-19 23:28:14 +0300

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,58 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+
+from dishlist.models import Place
+from users.forms import LoginUserForm, RegisterUserForm, ProfileUserForm, UserPasswordChangeForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'users/register_done.html')
+            #return render(request, 'dishlist/dashboard/welcome.html')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Авторизация'}
+
+    def get_success_url(self):
+
+        try:
+            place_id = Place.objects.values('pk').get(author=self.request.user)
+            pk = place_id['pk']
+            return reverse_lazy('dashboard_menu',
+                                kwargs={'place_id': pk})
+        except:
+            return reverse_lazy('add_place')
+
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = ProfileUserForm
+    template_name = 'users/profile.html'
+    extra_context = {'title': "Профиль пользователя"}
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile')
+
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class UserPasswordChange(PasswordChangeView):
+    form_class = UserPasswordChangeForm
+    success_url = reverse_lazy('users:password_change_done')
+    template_name = 'users/password_change_form.html'
