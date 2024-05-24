2a36e94931ce7e552bc310658cd89a128a8297c5
novoross86 <novoross86@gmail.com>
2024-05-24 10:41:52 +0300

    Success

--- mymanu/settings.py
+++ mymanu/settings.py
@@ -66,23 +66,23 @@ WSGI_APPLICATION = 'mymanu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test_pro',
-        'USER': 'postgres',
-        "PASSWORD": "1",
-        "HOST": "localhost",
-        "PORT": 5432,
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'test_pro', #test_pro
+         'USER': 'postgres',
+         "PASSWORD": "1", #1
+         "HOST": "localhost",
+         "PORT": '5432',
+     }
+ }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/home/media/'
 
 
 # Default primary key field type
